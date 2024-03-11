69ba2cb792029d24ba38b83f6d28504de11bc563
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2024-03-11 10:39:23 -0300

    make notification from body provided by request

--- src/notification.controller.ts
+++ src/notification.controller.ts
@@ -1,6 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { randomUUID } from 'node:crypto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+import { randomUUID } from 'crypto';
 import { PrismaService } from './prisma.service';
+
+class CreateNotificationBody {
+  @IsUUID()
+  @IsNotEmpty()
+  recipientId: string;
+
+  @IsNotEmpty()
+  @Length(5, 240)
+  content: string;
+
+  @IsNotEmpty()
+  category: string;
+}
 
 @Controller('/notifications')
 export class NotificationController {
@@ -12,14 +26,18 @@ export class NotificationController {
   }
 
   @Post()
-  async createNewNotification() {
-    await this.prismaService.notification.create({
-      data: {
-        id: randomUUID(),
-        content: 'Você tem uma nova solicitação de amizade',
-        category: 'social',
-        recipientId: randomUUID(),
-      },
+  async createNewNotification(@Body() body: CreateNotificationBody) {
+    const { content, category, recipientId } = body;
+    const newNotificationId = randomUUID();
+    const newNotification = {
+      id: newNotificationId,
+      content,
+      category,
+      recipientId,
+    };
+    const createdNotification = await this.prismaService.notification.create({
+      data: newNotification,
     });
+    return createdNotification;
   }
 }
